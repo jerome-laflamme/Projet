5f05431d636dda2eede306f89f35b683a5caf738
Jerome Laflamme <jeromelaflammep@gmail.com>
2022-03-12 13:17:02 -0500

    addition au main
    commencement du main

--- Program.cs
+++ Program.cs
@@ -7,12 +7,25 @@ namespace Projet
     {
         static void Main()
         {
+            int input;
+            decimal coins = 0;
             
-            Candy bonbonChoisi = GetCandy(getSelection()); // Enregistrer le bonbon choisi dans une variable de type Candy en demandant le numero du bonbon
             
-            Console.WriteLine(bonbonChoisi.Stock);
-            
-
+            while (true)
+            {
+                input = getSelection(); // Demander le numero de bonbon
+                Candy bonbonChoisi = GetCandy(input +1); // Enregistrer le bonbon choisi dans une variable de type Candy
+                if (bonbonChoisi.Stock < 1) {
+                    Board.Print( $"{bonbonChoisi.Name} VIDE!", selection:input); // Imprimer que le bonbon choisi est hors stock
+                }
+                else {
+                    Board.Print( bonbonChoisi.Name, input, bonbonChoisi.Price);
+                    while (bonbonChoisi.Price >= coins){
+                        
+                        coins += GetCoin(); // Demander d'ajouter de la monnaie jusqu'a ce que le montant demande soit atteint
+                    }
+                }
+            }
         }
 
         public static Candy[] loadCandies() // Fonction qui retourne un array de Candy 
@@ -27,9 +40,9 @@ namespace Projet
             Board.Print(); // Printer le board vierge
             
             int selection = 0;
-            while (selection < 1 || selection > choix)
+            while (selection < 1 || selection > choix) // s'assurer que le choix soit entre 1 et 25
             {
-                Console.Write("->");
+                Console.Write("-> ");
                 selection = int.Parse(Console.ReadLine());
             }
             return selection;
@@ -41,7 +54,7 @@ namespace Projet
             return bonbon[choixBonbon]; //Et retourner le bonbon linker avec le choix de l'utilisateur
         }
 
-        static float GetCoin()
+        static decimal GetCoin()
         {
             byte input;
             Console.WriteLine("[0] = Annuler");
@@ -61,17 +74,17 @@ namespace Projet
             switch (input) // input = montant
             {
               case  0:
-                  return 0;
+                  break;
               case 1:
-                  return 0.05f;
+                  return 0.05m;
               case 2:
-                  return 0.1f;
+                  return 0.1m;
               case 3:
-                  return 0.25f;
+                  return 0.25m;
               case 4:
-                  return 1f;
+                  return 1m;
               case 5:
-                  return 2f;
+                  return 2m;
             }
 
             return 0;
